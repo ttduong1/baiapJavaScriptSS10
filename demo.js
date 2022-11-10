c6eadafe513b6b104a44792a7604100f4e1bf158
duongt <trinhtungduong2019@gmail.com>
2022-11-11 05:30:07 +0700

    update

---
+++ demo.js
@@ -0,0 +1,7 @@
+function myif() {
+    document.getElementById("Physics points").innerHTML = prompt("Nhập điểm vật lý của mày");
+    document.getElementById("Chemistry score").innerHTML = prompt("Nhập điểm hóa học của mày");
+    document.getElementById("Biology score").innerHTML = prompt("Nhập điểm sinh học của mày");
+    document.getElementById("Medium score").innerHTML = prompt("Nhập điểm trung bình của mày");
+    document.getElementById("total score").innerHTML = prompt("Nhập tổng điểm của mày");
+}
---
+++ demo2.js
@@ -0,0 +1,1 @@
+document.getElementById("Poison").innerHTML = prompt("Nhập độ C");
